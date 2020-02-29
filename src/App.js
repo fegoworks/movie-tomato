421cac11703d73ce3d12305948fedda0afdc7d8e
unknown <fegoworks@hotmail.com>
2020-02-29 17:40:54 +0100

    chore(app-component): add movies to main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Movie from './components/movie';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <h1>Hello World</h1>
+        <Movie />
       </main>
     );
   }
