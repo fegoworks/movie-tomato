4ddf2abaafa07dea5a2b0c977c59d4adff56cd9b
unknown <fegoworks@hotmail.com>
2020-02-29 17:39:01 +0100

    feat(movie-component): create a movie list component

---
+++ src/components/movie.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import Like from './common/like';
+import { getMovies } from '../services/movieServices';
+
+class Movie extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  handleLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
+  'uuhxfgxjlysplpmbww@ttirv.org';
+  render() {
+    const { movies } = this.state;
+    if (movies.length === 0) {
+      return <p>There are no movies in the database</p>;
+    }
+    return (
+      <React.Fragment>
+        <p>Showing {movies.length} movies in the database.</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-small"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movie;
